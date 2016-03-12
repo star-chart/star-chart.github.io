41454c5b9fadf19837a18490903521d390b5283d
Zuzeng Lin <linzuzeng@hotmail.com>
2016-03-12 13:46:52 +0800

    first commit

---
+++ solar.js
@@ -0,0 +1,207 @@
+﻿(function (){
+
+//=================================行星星历=========================================
+//==================================================================================
+
+var XL0_xzb = new Array( //行星星历修正表
+ //经(角秒),纬(角秒), 距(10-6AU)
+ -0.08631, +0.00039, -0.00008,  //水星
+ -0.07447, +0.00006, +0.00017,  //金星
+ -0.07135, -0.00026, -0.00176,  //火星
+ -0.20239, +0.00273, -0.00347,  //木星
+ -0.25486, +0.00276, +0.42926,  //土星
+ +0.24588, +0.00345, -14.46266, //天王星
+ -0.95116, +0.02481, +58.30651  //海王星
+);
+
+
+function XL0_calc(xt,zn,t,n){ //xt星体,zn坐标号,t儒略世纪数,n计算项数
+  t/=10; //转为儒略千年数
+  var i,j,v=0,tn=1,c;
+  var F=XL0[xt],n1,n2,N;
+  var n0, pn=zn*6+1, N0 = F[pn+1]-F[pn]; //N0序列总数
+  for(i=0;i<6;i++,tn*=t){
+    n1=F[pn+i], n2=F[pn+1+i], n0=n2-n1;
+    if(!n0) continue;
+    if(n<0) N=n2;  //确定项数
+    else { N = int2(3*n*n0/N0+0.5)+n1;  if(i) N+=3;  if(N > n2) N=n2; }
+    for(j=n1,c=0;j<N;j+=3) c += F[j]*Math.cos(F[j+1] +t*F[j+2]);
+    v += c*tn;
+  }
+  v/=F[0];
+  if(xt==0){ //地球
+    var t2=t*t, t3=t2*t; //千年数的各次方
+    if(zn==0) v += (-0.0728 -2.7702*t -1.1019*t2 -0.0996*t3) / rad;
+    if(zn==1) v += (+0.0000 +0.0004*t +0.0004*t2 -0.0026*t3) / rad;
+    if(zn==2) v += (-0.0020 +0.0044*t +0.0213*t2 -0.0250*t3) / 1000000;
+  }else{ //其它行星
+    var dv = XL0_xzb[ (xt-1)*3+zn ];
+    if(zn==0) v += -3*t/rad;
+    if(zn==2) v += dv/1000000;
+    else      v += dv/rad;
+  }
+  return v;
+}
+
+function p_coord(xt,t,n1,n2,n3){ //xt星体,T儒略世纪数,TD
+ var z=new Array();
+ if(xt<8){
+  z[0]=XL0_calc(xt,0, t,n1);
+  z[1]=XL0_calc(xt,1, t,n2);
+  z[2]=XL0_calc(xt,2, t,n3);
+ }
+ if(xt==9){ //太阳
+   z[0]=0,z[1]=0,z[2]=0;
+ }
+ return z;
+}
+
+
+//=================================月亮星历--=======================================
+//==================================================================================
+
+function XL1_calc(zn,t,n){ //计算月亮
+  var ob=XL1[zn];
+  var i,j,F,N,v=0,tn=1,c;
+  var t2=t*t,t3=t2*t,t4=t3*t,t5=t4*t,tx=t-10;
+  if(zn==0){
+    v += (3.81034409 + 8399.684730072*t -3.319e-05*t2 + 3.11e-08*t3 - 2.033e-10*t4)*rad; //月球平黄经(弧度)
+    v += 5028.792262*t + 1.1124406*t2 + 0.00007699*t3 - 0.000023479*t4 -0.0000000178*t5;  //岁差(角秒)
+    if(tx>0) v += -0.866 +1.43*tx +0.054*tx*tx; //对公元3000年至公元5000年的拟合,最大误差小于10角秒
+  }
+  t2/=1e4,t3/=1e8,t4/=1e8;
+  n*=6; if(n<0) n = ob[0].length;
+  for(i=0;i<ob.length;i++,tn*=t){
+   F=ob[i];
+   N = int2( n*F.length/ob[0].length+0.5 );  if(i) N+=6;  if(N >= F.length) N=F.length;
+   for(j=0,c=0;j<N;j+=6) c+=F[j]*Math.cos(F[j+1] +t*F[j+2] +t2*F[j+3] +t3*F[j+4] +t4*F[j+5]);
+   v += c*tn;
+  }
+  if(zn!=2) v/=rad;
+  return v;
+};
+
+
+function m_coord(t,n1,n2,n3){ //返回月球坐标,n1,n2,n3为各坐标所取的项数
+  var re = new Array();
+  re[0] = XL1_calc( 0, t, n1 );
+  re[1] = XL1_calc( 1, t, n2 );
+  re[2] = XL1_calc( 2, t, n3 );
+  return re;
+}
+
+//==================================================================================
+
+var cs_rEar = 6378.1366; //地球赤道半径(千米)
+var cs_ba = 0.99664719; //地球极赤半径比
+var cs_AU   = 1.49597870691e8; //天文单位长度(千米)
+var rad  = 180*3600/Math.PI; //每弧度的角秒数
+var radd = 180/Math.PI; //每弧度的度数
+
+function rad2rrad(v){//对超过-PI到PI的角度转为-PI到PI
+  v=v % (2*Math.PI);
+  if(v<=-Math.PI) return v+2*Math.PI;
+  if(v>Math.PI) return v-2*Math.PI;
+  return v;
+}
+
+function rad2mrad(v){ //对超过0-2PI的角度转为0-2PI
+  v=v % (2*Math.PI);
+  if(v<0) return v+2*Math.PI;
+  return v;
+}
+
+function h2g(z,a){ //日心球面转地心球面,Z星体球面坐标,A地球球面坐标
+  //本含数是通用的球面坐标中心平移函数,行星计算中将反复使用
+  a = llr2xyz(a); //地球
+  z = llr2xyz(z); //星体
+  z[0]-=a[0]; z[1]-=a[1]; z[2]-=a[2];
+  return xyz2llr(z);
+}
+
+function llrConv(JW,E){ //球面坐标旋转
+  //黄道赤道坐标变换,赤到黄E取负
+  var r=new Array(),J=JW[0],W=JW[1];
+  r[0]=atan2( sin(J)*cos(E) - tan(W)*sin(E), cos(J) );
+  r[1]=asin ( cos(E)*sin(W) + sin(E)*cos(W)*sin(J)  );
+  r[2]=JW[2];
+  r[0]=rad2mrad(r[0]);
+  return r;
+}
+
+function parallax(z,H,fa,high){ //视差修正
+  //z赤道坐标,fa地理纬度,H时角,high海拔(千米)
+  var dw=1; if(z[2]<500) dw=cs_AU;
+  z[2]*=dw;
+  var r0,x0,y0,z0, f=cs_ba, u=Math.atan(f*Math.tan(fa)), g=z[0]+H;
+  r0 = cs_rEar*Math.cos(u)  + high*Math.cos(fa); //站点与地地心向径的赤道投影长度
+  z0 = cs_rEar*Math.sin(u)*f+ high*Math.sin(fa); //站点与地地心向径的轴向投影长度
+  x0 = r0*Math.cos(g);
+  y0 = r0*Math.sin(g);
+  var s = llr2xyz(z);
+  s[0]-=x0, s[1]-=y0, s[2]-=z0;
+  s = xyz2llr(s);
+  z[0]=s[0], z[1]=s[1], z[2]=s[2]/dw;
+}
+
+function llr2xyz(JW){ //球面转直角坐标
+  var r=new Array(),J=JW[0],W=JW[1],R=JW[2];
+  r[0]=R*cos(W)*cos(J);
+  r[1]=R*cos(W)*sin(J);
+  r[2]=R*sin(W);
+  return r;
+}
+
+function xyz2llr(xyz){ //直角坐标转球
+  var r=new Array(), x=xyz[0],y=xyz[1],z=xyz[2];
+  r[2] = sqrt(x*x+y*y+z*z);
+  r[1] = asin(z/r[2]);
+  r[0] = rad2mrad( atan2(y,x) );
+  return r;
+}
+
+function xingX(xt){ //行星计算,jd力学时
+	var z,a;
+	if(xt==10)
+	{
+		z  = m_coord(T,-1,-1,-1);
+		//z[0] = rad2mrad(z[0]); 
+		z = llrConv(z,E);
+	}
+	if(xt<10)
+	{
+		a = p_coord(0, T,-1,-1,-1);
+		z = p_coord(xt,T,-1,-1,-1);
+		z[0] = rad2mrad(z[0]);
+		z = h2g(z,a);
+		z[0] = rad2mrad(z[0]);
+		z = llrConv(z,E);
+	}
+	var sj = rad2rrad(lst*15*Math.PI/180 - z[0]);
+	parallax(z, sj,phy/180*Math.PI,0);
+	return z;
+}
+
+
+xingxing = function ()
+{
+	var z=xingX(9)
+	ss[0]=z[0];
+	ss[1]=z[1];
+	ss[2]=z[2];
+	for (var i=1;i<8;i++)
+	{
+		z=xingX(i)
+		ss[3*i+0]=z[0];
+		ss[3*i+1]=z[1];
+		ss[3*i+2]=z[2];
+	}
+	z=xingX(10)
+	ss[24]=z[0];
+	ss[25]=z[1];
+	ss[26]=z[2];
+}
+
+
+
+})();
---
+++ star.js
@@ -0,0 +1,800 @@
+﻿
+
+(function (){
+
+
+
+function int2(v) { return Math.floor(v); }
+function sqrt(x) { return Math.sqrt(x);  }
+function floor(x){ return Math.floor(x); }
+function abs(x)  { return Math.abs(x);   }
+function sin(x)  { return Math.sin(x);   }
+function cos(x)  { return Math.cos(x);   }
+function tan(x)  { return Math.tan(x);   }
+function cot(x)  { return 1/tan(x);      }
+function asin(x) { return Math.asin(x);  }
+function acos(x) { return Math.acos(x);  }
+function atan(x) { return Math.atan(x);  }
+function atan2(y,x){ return Math.atan2(y,x); }
+var pi=Math.PI;
+
+function mat299(a,b)
+{
+	var c=new Array(
+	a[0]*b[0]+a[1]*b[3]+a[2]*b[6],
+	a[0]*b[1]+a[1]*b[4]+a[2]*b[7],
+	a[0]*b[2]+a[1]*b[5]+a[2]*b[8],
+	a[3]*b[0]+a[4]*b[3]+a[5]*b[6],
+	a[3]*b[1]+a[4]*b[4]+a[5]*b[7],
+	a[3]*b[2]+a[4]*b[5]+a[5]*b[8],
+	a[6]*b[0]+a[7]*b[3]+a[8]*b[6],
+	a[6]*b[1]+a[7]*b[4]+a[8]*b[7],
+	a[6]*b[2]+a[7]*b[5]+a[8]*b[8])
+	return c;
+}
+
+function mat399(a,b,c)
+{
+	return mat299(a,mat299(b,c));
+}
+
+function setcolor()
+{
+	var sun_h=sin(phy/180*pi)*sin(ss[1])+cos(phy/180*pi)*cos(ss[1])*cos(lst/12*pi-ss[0])
+	if (flag==0||changecolor1==1)
+	{
+		rgb0=new Array(0,0,0);
+		rgb1=new Array(255-rgb0[0],255-rgb0[1],255-rgb0[2]);
+		colordata=new Array(
+		"#0000C0",//0赤道网格
+		"#800000",//1地平网格
+		"#808000",//2黄道
+		"#C0C0C0",//3地平线
+		"#808080",//4小星名
+		"#FFFFFF",//5大星名
+		"#A0A0A0",//6星座名
+		"#606060",//7星座连线
+		"#00FFFF",//8方位
+		"#808080",//9logo
+		"#FFFFFF",//10月亮前景
+		"#000000",//11月亮背景		
+		""
+		);
+	}
+	else if (sun_h<0)
+	{
+		rgb0=new Array(0,0,50);
+		rgb1=new Array(255-rgb0[0],255-rgb0[1],255-rgb0[2]);
+		colordata=new Array(
+		"#0000C0",//0赤道网格
+		"#C00000",//1地平网格
+		"#C0C000",//2黄道
+		"#C0C0C0",//3地平线
+		"#808080",//4小星名
+		"#FFFFFF",//5大星名
+		"#808080",//6星座名
+		"#404040",//7星座连线
+		"#00FFFF",//8方位
+		"#808080",//9logo
+		"#FFFF00",//10月亮前景
+		"#000040",//11月亮背景	
+		""
+		);
+	}else{
+		rgb0=new Array(50,100,255);
+		rgb1=new Array(255-rgb0[0],255-rgb0[1],255-rgb0[2]);
+		colordata=new Array(
+		"#4040C0",//0赤道网格
+		"#804040",//1地平网格
+		"#808000",//2黄道
+		"#202020",//3地平线
+		"#404040",//4小星名
+		"#000000",//5大星名
+		"#202020",//6星座名
+		"#60A0FF",//7星座连线
+		"#0000C0",//8方位
+		"#808080",//9logo
+		"#C0D0F0",//10月亮前景
+		"#4060FF",//11月亮背景	
+		""
+		);
+	}
+	if (changecolor1==2)
+	{
+		rgb0=new Array(255,255,255);
+		rgb1=new Array(-255,-255,-255);
+		colordata=new Array(
+		"#404040",//0赤道网格
+		"#404040",//1地平网格
+		"#303030",//2黄道
+		"#000000",//3地平线
+		"#404040",//4小星名
+		"#000000",//5大星名
+		"#202020",//6星座名
+		"#808080",//7星座连线
+		"#101010",//8方位
+		"#808080",//9logo
+		"#000000",//10月亮前景
+		"#FFFFFF",//11月亮背景	
+		""
+		);
+	}
+}
+
+var rgb0=new Array(3);
+var rgb1=new Array(3);
+var colordata=new Array(13);
+
+function clear()
+{
+	for (var i=0;i<imgData.data.length;i+=4)
+	{
+		imgData.data[i]=rgb0[0];
+		imgData.data[i+1]=rgb0[1];
+		imgData.data[i+2]=rgb0[2];
+		imgData.data[i+3]=255;
+	}
+}
+
+function logo()
+{
+	cxt.font="15px 宋体";
+	cxt.fillStyle=colordata[9];
+	cxt.fillText("MADE by 北洋星语 Zhu1995zpb",10,sizey-10);
+}
+
+drawstar = function (x,y,r,rgb)
+{
+	if (changecolor1==2){
+
+	var x1=floor(x-r);
+	var x2=floor(x+r)+1;
+	var y1=floor(y-r);
+	var y2=floor(y+r)+1;
+	var x0=x2-x1+1;
+	var y0=y2-y1+1;
+	for (var j=x1;j<x1+x0;j++){
+		for (var k=y1;k<y1+y0;k++){
+			var ll=((j-x)*(j-x)+(k-y)*(k-y))/(r*r);
+			if (ll<1){
+				ll=1-ll*ll*ll*ll;
+				imgData.data[4*j+k*size0x*4+0]+=rgb1[0]*ll*mag2;
+				imgData.data[4*j+k*size0x*4+1]+=rgb1[1]*ll*mag2;
+				imgData.data[4*j+k*size0x*4+2]+=rgb1[2]*ll*mag2;
+				imgData.data[4*j+k*size0x*4+3]=255;
+			}
+		}
+	}
+
+	}else{
+
+	var x1=floor(x-r);
+	var x2=floor(x+r)+1;
+	var y1=floor(y-r);
+	var y2=floor(y+r)+1;
+	var x0=x2-x1+1;
+	var y0=y2-y1+1;
+	for (var j=x1;j<x1+x0;j++){
+		for (var k=y1;k<y1+y0;k++){
+			var ll=((j-x)*(j-x)+(k-y)*(k-y))/(r*r);
+			if (ll<1){
+				ll=1-ll*ll*ll;
+				imgData.data[4*j+k*size0x*4+0]+=rgb1[0]*rgb[0]*ll*mag2;
+				imgData.data[4*j+k*size0x*4+1]+=rgb1[1]*rgb[1]*ll*mag2;
+				imgData.data[4*j+k*size0x*4+2]+=rgb1[2]*rgb[2]*ll*mag2;
+				imgData.data[4*j+k*size0x*4+3]=255;
+			}
+		}
+	}
+
+	}
+}
+
+
+function Rmat(flag,a1,a2,a3)
+{
+
+if(flag==1)
+{
+	var A=a1;
+	var h=a2;
+	var c=a3;
+	var a=-A;
+	var b=-90+h;
+	var c=c;
+	a=a/180*pi;
+	b=b/180*pi;
+	c=c/180*pi;
+	var R1=new Array(cos(a),sin(a),0,-sin(a),cos(a),0,0,0,1);
+	var R2=new Array(1,0,0,0,cos(b),sin(b),0,-sin(b),cos(b));
+	var R3=new Array(cos(c),sin(c),0,-sin(c),cos(c),0,0,0,1);
+	var Rx=mat399(R3,R2,R1);
+	a=6+lst;
+	b=(90-phy);
+	c=0;
+	a=a*15/180*pi;
+	b=b/180*pi;
+	c=c/180*pi;
+	var R1=new Array(cos(a),sin(a),0,-sin(a),cos(a),0,0,0,1);
+	var R2=new Array(1,0,0,0,cos(b),sin(b),0,-sin(b),cos(b));
+	var R3=new Array(cos(c),0,-sin(c),0,1,0,sin(c),0,cos(c));
+	var Ry=mat399(R3,R2,R1);
+	R=mat299(Rx,Ry);
+	Rz=Rx;
+	return R;
+}
+
+if(flag==0)
+{
+	var RA=a1;
+	var Dec=a2;
+	var o=a3;
+	var a=RA;
+	var b=Dec;
+	a=a*15-90;
+	b=-90+b;
+	var c=o;
+	a=a/180*pi;
+	b=b/180*pi;
+	c=c/180*pi;
+	var R1=new Array(cos(a),sin(a),0,-sin(a),cos(a),0,0,0,1);
+	var R2=new Array(1,0,0,0,cos(b),sin(b),0,-sin(b),cos(b));
+	var R3=new Array(cos(c),sin(c),0,-sin(c),cos(c),0,0,0,1);
+	var Rx=mat399(R3,R2,R1);
+	R=Rx;
+	return R;
+}
+
+}
+
+function Rmat1()
+{
+	var a=direction_current[0];
+	var b=direction_current[1];
+	var c=direction_current[2];
+	a=a/180*pi;
+	b=b/180*pi;
+	c=c/180*pi;
+	x2=atan2(-cos(a)*sin(c)-sin(a)*sin(b)*cos(c),-sin(a)*sin(c)+cos(a)*sin(b)*cos(c))/pi*180;
+	o=atan2(cos(b)*sin(c),sin(b))/pi*180;
+	y2=-asin(cos(b)*cos(c))/pi*180;
+	if(x2<0)x2+=360;
+}
+
+var rgb=new Array(0,0,0);
+
+var stardata3= new Array(30000);
+{
+	for (var i=0;i<10000;i++){
+		var RA=RD[2*i];
+		var Dec=RD[2*i+1];
+		var a=RA*15/180*pi;
+		var b=Dec/180*pi;
+		stardata3[3*i+0]=cos(a)*cos(b);
+		stardata3[3*i+1]=sin(a)*cos(b);
+		stardata3[3*i+2]=sin(b);
+	}
+
+}
+
+var mag2;
+
+starmag = function starmag(c)
+{
+	if (c<1){
+		c=(c-1)/2.5+1;
+	}
+	var mag1=star_size1;
+	mag2=Math.pow(0.4,c-star_size2);
+	if (mag2>1){
+		mag1=mag1*Math.sqrt(mag2);
+		mag2=1;
+	}
+	r=mag1*z1/2*size/1000*sqrt(fov/30);
+	return r;
+}
+
+var starlog= new Array(40000);
+
+var c,d,z,l,x,y,r;
+var star_n;
+
+function findstar()
+{
+	if(planet)
+		findstars();
+	star_n=0;
+	for (var i=0;i<10000;i++){
+		c=mag[i];
+		d=color_star[i];
+		z=new Array(stardata3[3*i+0],stardata3[3*i+1],stardata3[3*i+2])
+		l=1+R[6]*z[0]+R[7]*z[1]+R[8]*z[2];
+		if (l>z2)
+		{
+			x=R[0]*z[0]+R[1]*z[1]+R[2]*z[2];
+			y=R[3]*z[0]+R[4]*z[1]+R[5]*z[2];
+			x=(x/l*size*z1+sizex)/2;
+			y=(y/l*size*z1+sizey)/2;
+			if (x<sizex&&x>0&&y<sizey&&y>0)
+			{
+				rgb[0]=color_data[3*d+0];
+				rgb[1]=color_data[3*d+1];
+				rgb[2]=color_data[3*d+2];
+				drawstar(x+size1,y+size1,starmag(c),rgb);
+				starlog[4*star_n+0]=i;
+				starlog[4*star_n+1]=x;
+				starlog[4*star_n+2]=y;
+				starlog[4*star_n+3]=r;
+				star_n++;
+			}
+		}
+	}
+}
+
+function starname()
+{
+	if(planet)
+		starnames();
+	cxt.font="15px 宋体";
+	cxt.fillStyle=colordata[4];
+	for (var i=0;mag[starlog[4*i+0]]<(90/(fov+5));i++){
+		cxt.fillText(name1[starlog[4*i+0]],starlog[4*i+1]+starlog[4*i+3]+2,starlog[4*i+2]+starlog[4*i+3]);
+	}
+}
+
+choosestar = function (x,y)
+{
+	if(planet)
+		choosestars(x,y);
+	for (var i=0;i<star_n;i++){
+		var x1=starlog[4*i+1];
+		var y1=starlog[4*i+2];
+		var r=starlog[4*i+3];
+		var r1=5;
+		if (r>r1)
+			r1=r;
+		if (sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y))<r1)
+		{
+			cxt.font="25px 黑体";
+			cxt.fillStyle=colordata[5];
+			cxt.fillText(name1[starlog[4*i+0]]+"  "+name2[starlog[4*i+0]]+"  "+mag[starlog[4*i+0]],name_x,name_y);
+			cxt.font="15px 宋体";
+			cxt.fillStyle=colordata[5];
+			cxt.fillText(name1[starlog[4*i+0]],x1+r+2,y1+r);
+			name_y+=35;
+		}
+	}
+}
+
+var be;
+
+function drawline()
+{
+	var dx=0.05;
+	var dy=0.5;
+	var d0=360/(floor(360/(40*2*fov/size)))/15;
+	var d1=160/(floor(160/(40*2*fov/size)));
+	if (d0>0.5)d0=0.5;
+	if (d1>10)d1=10;
+
+	if (gird_eq){
+
+		cxt.beginPath();
+		cxt.strokeStyle=colordata[0];
+		cxt.lineWidth=starmag(star_size2)/2;
+
+		for (var j=-80;j<90;j+=10)
+		{
+			be=0;
+			for (var i=-12;i-dx<=12;i+=d0)
+			{
+				drawline1(i*15,j,R);
+			}
+			be=0;
+		}
+
+		for (var i=0;i<24;i+=1)
+		{
+			be=0;
+			for (var j=-80;j-dy<=80;j+=d1)
+			{
+				drawline1(i*15,j,R);
+			}
+			be=0;
+		}
+
+		cxt.stroke();
+
+	}
+
+	if (flag&&gird_dp){
+
+		cxt.beginPath();
+		cxt.strokeStyle=colordata[1];
+		cxt.lineWidth=starmag(star_size2)/2;
+
+		for (var j=-80;j<90;j+=10)
+		{
+			be=0;
+				for (var i=-12;i-dx<=12;i+=d0)
+			{
+				drawline1(i*15,j,Rz);
+			}
+			be=0;
+		}
+
+		for (var i=0;i<24;i+=1)
+		{
+			be=0;
+			for (var j=-80;j-dy<=80;j+=d1)
+			{
+				drawline1(i*15,j,Rz);
+			}
+			be=0;
+		}
+
+		cxt.stroke();
+
+	}
+
+	if (gird_ec){
+
+		cxt.beginPath();
+		cxt.strokeStyle=colordata[2];
+		cxt.lineWidth=1;
+
+		be=0;
+		for (var i=-12;i-dx<=12;i+=d0)
+		{
+			var a1=(i*15)/180*pi;
+			var a=atan2( sin(a1)*cos(E), cos(a1) );
+			var b=asin ( sin(E)*sin(a1) );
+			drawline1(a*180/pi,b*180/pi,R);
+		}
+		be=0;
+
+		cxt.stroke();
+
+	}
+
+	if (flag&&fangwei){
+
+		cxt.beginPath();
+		cxt.strokeStyle=colordata[3];
+		cxt.lineWidth=10;
+
+		for (var j=0;j<1;j+=1)
+		{
+			be=0;
+			for (var i=-12;i-dx<=12;i+=d0/10)
+			{
+				drawline1(i*15,j,Rz);
+			}
+			be=0;
+		}
+
+		cxt.stroke();
+
+	}
+
+}
+
+function drawfangwei()
+{
+	var fangxiangdata=new Array(0,0,1,0,0,-1,0,1,0,0,-1,0,1,0,0,-1,0,0);
+	var fangxiangname=new Array('天顶','天底','北','南','东','西')
+
+	cxt.font="30px 黑体";
+	cxt.fillStyle=colordata[8];
+	for (var j=0;j<6;j++){
+		var z=new Array(fangxiangdata[3*j+0],fangxiangdata[3*j+1],fangxiangdata[3*j+2])
+		var l=1+Rz[6]*z[0]+Rz[7]*z[1]+Rz[8]*z[2];
+		if (l>z2-0.1)
+		{
+			var x=Rz[0]*z[0]+Rz[1]*z[1]+Rz[2]*z[2];
+			var y=Rz[3]*z[0]+Rz[4]*z[1]+Rz[5]*z[2];
+			x=(x/l*size*z1+sizex)/2;
+			y=(y/l*size*z1+sizey)/2;
+			var dy=j>1?10:-15;
+			cxt.fillText(fangxiangname[j],x-15*fangxiangname[j].length,y-dy);
+		}
+	}
+}
+
+
+var line_c_n=1348;
+var linec=new Array(line_c_n);
+
+function drawline2()
+{
+	var r0=starmag(star_size2);
+	var r1,r2;
+	cxt.beginPath();
+	cxt.strokeStyle=colordata[7];
+	cxt.lineWidth=r0;
+	for (var j=0;j<line_c_n;j++)
+	{
+		linec[j]=0;
+		var i=line_c[j]-1;
+		z=new Array(stardata3[3*i+0],stardata3[3*i+1],stardata3[3*i+2])
+		l=1+R[6]*z[0]+R[7]*z[1]+R[8]*z[2];
+		if (l>z2-0.3)
+		{
+			x=R[0]*z[0]+R[1]*z[1]+R[2]*z[2];
+			y=R[3]*z[0]+R[4]*z[1]+R[5]*z[2];
+			x=(x/l*size*z1+sizex)/2;
+			y=(y/l*size*z1+sizey)/2;
+			if (j%2==0)
+			{
+				var x1=x;var y1=y;
+				linec[j]=1;
+				r1=starmag(mag[i])+2*r0;
+			}
+			if (j%2==1)
+			{
+				if(linec[j-1]==1)
+				{
+					var x2=x;var y2=y;r2=starmag(mag[i])+2*r0;			
+					var k0=sqrt((y2-y1)*(y2-y1)+(x2-x1)*(x2-x1));
+					if (k0>(r1+r2))
+					{
+						var k1=(x2-x1)/k0;
+						var k2=(y2-y1)/k0;
+						x1+=r1*k1;x2-=r2*k1;
+						y1+=r1*k2;y2-=r2*k2;
+						cxt.moveTo(x1,y1);
+						cxt.lineTo(x2,y2);
+					}
+				}
+			}
+		}
+	}
+	cxt.stroke();
+}
+
+function drawline1(aa,bb,R0)
+{		a=aa/180*pi;
+		b=bb/180*pi;
+		z=new Array(cos(a)*cos(b),sin(a)*cos(b),sin(b));
+		l=1+R0[6]*z[0]+R0[7]*z[1]+R0[8]*z[2];
+		if (l>z2-0.3)
+		{
+			x=R0[0]*z[0]+R0[1]*z[1]+R0[2]*z[2];
+			y=R0[3]*z[0]+R0[4]*z[1]+R0[5]*z[2];
+			x=(x/l*size*z1+sizex)/2;
+			y=(y/l*size*z1+sizey)/2;
+			if (be==0)
+			{
+				be=1;
+				cxt.moveTo(x,y);
+			}else{
+				cxt.lineTo(x,y);
+			}
+		}else{
+		be=0;
+		}
+}
+
+var condata1= new Array(88*3);
+{
+	for (var i=0;i<88;i++){
+		var a=condata[2*i+1]*15/180*pi;
+		var b=condata[2*i+0]/180*pi;
+		condata1[3*i+0]=cos(a)*cos(b);
+		condata1[3*i+1]=sin(a)*cos(b);
+		condata1[3*i+2]=sin(b);
+	}
+}
+
+function drawconname()
+{
+	cxt.font="20px 黑体";
+	cxt.fillStyle=colordata[6];
+	for (var j=0;j<88;j++){
+		z=new Array(condata1[3*j+0],condata1[3*j+1],condata1[3*j+2])
+		l=1+R[6]*z[0]+R[7]*z[1]+R[8]*z[2];
+		if (l>z2-0.1)
+		{
+			x=R[0]*z[0]+R[1]*z[1]+R[2]*z[2];
+			y=R[3]*z[0]+R[4]*z[1]+R[5]*z[2];
+			x=(x/l*size*z1+sizex)/2;
+			y=(y/l*size*z1+sizey)/2;
+			cxt.fillText(conname[4*j+2],x-10*conname[4*j+2].length,y+10);
+		}
+	}
+
+}
+
+var color_datas= new Array(1.0, 0.98, 0.97,1.0, 0.964, 0.914,1., 0.96, 0.876,1., 0.768, 0.504 ,1., 0.983, 0.934,1.0, 0.955, 0.858,0.837, 0.959, 1.,0.44, 0.582, 1.,1., 0.986, 0.968);
+var mags= new Array(-8,-1,-2,-1,-2,-1,5,5,-4);
+var rs= new Array(0.25,10/60,20/60,15/60,25/60,15/60,5/60,5/60,0.25);
+var name1s=new Array('太阳','水星','金星','火星','木星','土星','天王星','海王星','月亮');
+var starlogs= new Array(36);
+var star_ns;
+ss=new Array(27);
+
+function findstars()
+{
+	starlogs= new Array(36);
+	star_ns=0;
+	for (var i=0;i<9;i++){
+		c=mags[i];
+		a=ss[3*i+0];
+		b=ss[3*i+1];
+		z=new Array(cos(a)*cos(b),sin(a)*cos(b),sin(b));
+		l=1+R[6]*z[0]+R[7]*z[1]+R[8]*z[2];
+		if (l>z2)
+		{
+			x=R[0]*z[0]+R[1]*z[1]+R[2]*z[2];
+			y=R[3]*z[0]+R[4]*z[1]+R[5]*z[2];
+			x=(x/l*size*z1+sizex)/2;
+			y=(y/l*size*z1+sizey)/2;
+			if (x<sizex&&x>0&&y<sizey&&y>0)
+			{
+				r=starmag(c);
+				if (r>10)r=10+(r-10)/3;
+				if (i*(i-8)==0&&fov<30)r=size*sqrt(2)*rs[i]/(2*fov);
+				if (fov<30)r=size*sqrt(2)*rs[i]/(2*fov);
+				rgb[0]=color_datas[3*i+0];
+				rgb[1]=color_datas[3*i+1];
+				rgb[2]=color_datas[3*i+2];
+				if(i!=8)drawstar(x+size1,y+size1,r,rgb);
+				starlogs[4*star_ns+0]=i;
+				starlogs[4*star_ns+1]=x;
+				starlogs[4*star_ns+2]=y;
+				starlogs[4*star_ns+3]=r;
+				star_ns++;
+			}
+		}
+	}
+}
+
+function drawmoon()
+{	var as,bs,am,bm,zs,zm,ls,lm,xm,xs,ym,ys,ra1,ea,r,xa1,ya1,xa2,ya2,xs2,ys2,k2,k1,k3,xa3,ya3,t1,t2,t3,xm1,ym1,tha,xa,ya;
+	am=ss[24];bm=ss[25];
+	zm=new Array(cos(am)*cos(bm),sin(am)*cos(bm),sin(bm));
+	lm=1+R[6]*zm[0]+R[7]*zm[1]+R[8]*zm[2];
+	xm=R[0]*zm[0]+R[1]*zm[1]+R[2]*zm[2];
+	ym=R[3]*zm[0]+R[4]*zm[1]+R[5]*zm[2];
+	as=ss[0];bs=ss[1];
+	zs=new Array(cos(as)*cos(bs),sin(as)*cos(bs),sin(bs));
+	ls=1+R[6]*zs[0]+R[7]*zs[1]+R[8]*zs[2];
+	xs=R[0]*zs[0]+R[1]*zs[1]+R[2]*zs[2];
+	ys=R[3]*zs[0]+R[4]*zs[1]+R[5]*zs[2];
+	ra1=sqrt((zs[0]-zm[0])*(zs[0]-zm[0])+(zs[1]-zm[1])*(zs[1]-zm[1])+(zs[2]-zm[2])*(zs[2]-zm[2]));
+	ea=-cos(acos(ra1/2)*2);
+	xm/=lm;ym/=lm;xs/=ls;ys/=ls;
+	r=starmag(mags[8]);
+	if (r>10)r=10+(r-10)/3;
+	if (fov<30)r=size*sqrt(2)*rs[8]/(2*fov);
+
+	xa1=(xm+xs)/2;
+	ya1=(ym+ys)/2;
+	ra1=-1/(xs*xs+ys*ys);
+	xs2=xs*ra1;
+	ys2=ys*ra1;
+	xa2=(xs+xs2)/2;
+	ya2=(ys+ys2)/2;
+	k2=(ys2-ys)/(xs2-xs);
+	k1=(ys-ym)/(xs-xm);
+	k1=-1/k1;
+	k2=-1/k2;
+	xa3=(ya1-ya2+k2*xa2-k1*xa1)/(k2-k1);
+	ya3=ya2+k2*(xa3-xa2);
+	k3=-1/((ya3-ym)/(xa3-xm));
+	t1=atan2(ys-ym,xs-xm);
+	t2=atan2(ym-ys2,xm-xs2);
+	t3=atan(k3);
+	if(t3>pi)t3=t3-2*pi;
+	if(t2<t1){var t0=t2;t2=t1;t1=t0}
+	if(t2-t1<pi)if(t3>t2||t3<t1)t3+=pi;
+	if(t2-t1>pi)if(t3<t2&&t3>t1)t3+=pi;
+	if(t3>pi)t3=t3-2*pi;
+
+	xm1=(xm*size*z1+sizex)/2;
+	ym1=(ym*size*z1+sizey)/2;
+
+	cxt.strokeStyle=colordata[11];
+	cxt.fillStyle=colordata[11];
+	cxt.beginPath();
+	cxt.lineWidth=0.1;
+
+	for (var i=0;i<100;i++){
+		ya=ym1+r*sin(i/100*2*pi);
+		xa=xm1+r*cos(i/100*2*pi);
+		if(i==0)cxt.moveTo(xa,ya);
+		cxt.lineTo(xa,ya);
+	}
+
+	cxt.closePath();
+	cxt.fill();
+	cxt.stroke();
+	cxt.strokeStyle=colordata[10];
+	cxt.fillStyle=colordata[10];
+	cxt.beginPath();
+	cxt.lineWidth=0.1;
+
+	tha=t3+pi/2;
+	for (var i=0;i<50;i++){
+		ya=ym1+r*sin(i/100*2*pi+tha+pi);
+		xa=xm1+r*cos(i/100*2*pi+tha+pi);
+		if(i==0)cxt.moveTo(xa,ya);
+		cxt.lineTo(xa,ya);
+	}
+	for (var i=0;i<50;i++){
+		ya=ym1+r*(cos(i/100*2*pi)*sin(tha)-ea*sin(i/100*2*pi)*cos(tha));
+		xa=xm1+r*(cos(i/100*2*pi)*cos(tha)+ea*sin(i/100*2*pi)*sin(tha));
+		cxt.lineTo(xa,ya);
+	}
+
+	cxt.closePath();
+	cxt.fill();
+	cxt.stroke();
+
+}
+
+function starnames()
+{
+	cxt.font="15px 宋体";
+	cxt.fillStyle=colordata[4];
+	for (var i=0;i<star_ns;i++){
+		cxt.fillText(name1s[starlogs[4*i+0]],starlogs[4*i+1]+starlogs[4*i+3]+2,starlogs[4*i+2]+starlogs[4*i+3]);
+	}
+}
+
+function choosestars(x,y)
+{
+	for (var i=0;i<star_ns;i++){
+		var x1=starlogs[4*i+1];
+		var y1=starlogs[4*i+2];
+		var r=starlogs[4*i+3];
+		var r1=5;
+		if (r>r1)
+			r1=r;
+		if (sqrt((x1-x)*(x1-x)+(y1-y)*(y1-y))<r1)
+		{
+			cxt.font="25px 黑体";
+			cxt.fillStyle=colordata[5];
+			cxt.fillText(name1s[starlogs[4*i+0]],name_x,name_y);
+			cxt.font="15px 宋体";
+			cxt.fillStyle=colordata[5];
+			cxt.fillText(name1s[starlogs[4*i+0]],x1+r+2,y1+r);
+			name_y+=35;
+		}		
+	}
+}
+
+
+
+star = function ()
+{
+	setcolor();
+	clear();
+	if(isdirection)
+		Rmat1();
+	if (flag==1)
+		findstar(Rmat(flag,x2,y2,o));
+	if (flag==0)
+		findstar(Rmat(flag,x1,y1,o));
+	cxt.putImageData(imgData,-size1,-size1);
+
+	if (gird_eq||gird_dp||gird_ec||fangwei)
+		drawline();
+
+	if(con_line)
+		drawline2();
+	if(con_name)
+		drawconname();
+	if(planet)
+		drawmoon();
+	if(star_name)
+		starname();
+
+	if (fangwei&&flag)
+		drawfangwei();
+
+	logo();
+}
+
+
+})();
